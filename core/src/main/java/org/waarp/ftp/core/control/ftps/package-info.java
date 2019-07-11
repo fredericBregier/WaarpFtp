3b8630496b8e9edf958679776fa54b3f6add4d1f
Frederic Bregier <frederic.bregier@waarp.fr>
2019-07-11 11:14:12 +0200

    Fix pom

---
+++ core/src/main/java/org/waarp/ftp/core/control/ftps/package-info.java
@@ -0,0 +1,30 @@
+/**
+ * This file is part of Waarp Project.
+ * <p>
+ * Copyright 2009, Frederic Bregier, and individual contributors by the @author tags. See the COPYRIGHT.txt in the
+ * distribution for a full listing of individual contributors.
+ * <p>
+ * All Waarp Project is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * <p>
+ * Waarp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with Waarp . If not, see
+ * <http://www.gnu.org/licenses/>.
+ * <p>
+ * This module is for FTPS support as native SSL/TLS on port 990 similar to RFC 2818.
+ *
+ * @author "Frederic Bregier"
+ * <p>
+ * This module is for FTPS support as native SSL/TLS on port 990 similar to RFC 2818.
+ * @author "Frederic Bregier"
+ */
+/**
+ * This module is for FTPS support as native SSL/TLS on port 990 similar to RFC 2818.
+ *
+ * @author "Frederic Bregier"
+ *
+ */
+package org.waarp.ftp.core.control.ftps;
