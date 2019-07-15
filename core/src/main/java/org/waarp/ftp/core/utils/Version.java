87158f83b50f3d56367ba39162d38a6066f17f53
Frederic Bregier <frederic.bregier@waarp.fr>
2019-07-15 12:19:24 +0200

    Fix Pom

--- core/src/main/java/org/waarp/ftp/core/utils/Version.java
+++ core/src/main/java/org/waarp/ftp/core/utils/Version.java
@@ -5,19 +5,17 @@ package org.waarp.ftp.core.utils;
  * Provides the version information of WaarpFtp.
  */
 public final class Version {
-  /**
-   * The version identifier.
-   */
-  public static final String ID = "3.0.8";
-
-  private Version() {
-    super();
-  }
-
-  /**
-   * Prints out the version identifier to stdout.
-   */
-  public static void main(String[] args) {
-    System.out.println(ID);
-  }
+ /**
+ * The version identifier.
+ */
+ public static final String ID = "3.0.8";
+ /**
+ * Prints out the version identifier to stdout.
+ */
+ public static void main(String[] args) {
+     System.out.println(ID);
+ }
+ private Version() {
+     super();
+ }
 }
